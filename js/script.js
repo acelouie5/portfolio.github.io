21e4065b81576eb91c33ee5d175e8c82e9434d82
acelouie5 <79086112+acelouie5@users.noreply.github.com>
2023-06-19 22:09:38 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzAAcCXz4MwBqh8JOF0WjbEnxXCvRnTG-1XXhyJsJ5nPtn-DLTyXtloJ1PaDnyRHtoDbA/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message sent successfully"
+            setTimeout(function()
+            {
+                msg.innerHTML =""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
+
+    var tablinks = document.getElementsByClassName("tab-links");
+        var tabcontents = document.getElementsByClassName("tab-contents");
+        function opentab(tabname)
+        {
+            for(tablink of tablinks)
+            {
+                tablink.classList.remove("active-link");
+            }
+            for(tabcontent of tabcontents)
+            {
+                tabcontent.classList.remove("active-content");
+            }
+            event.currentTarget.classList.add("active-link")
+            document.getElementById(tabname).classList.add("active-content")
+        }
+
+
+        var sidemenu = document.getElementById("sidemenu");
+
+        function openmenu()
+        {
+            sidemenu.style.right = "0";
+
+        }
+        function closemenu()
+        {
+            sidemenu.style.right = "-200px";
+            
+        }
